138002170e1162d1a5703eeb56394f1a71195faf
itarek99 <tarekulislam7060@gmail.com>
2023-01-27 23:54:58 +0600

    fetch single data from firebase

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,4 +1,30 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { db } from '../firebase/firebase.config';
+
 const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLink, setShareLink] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listing', params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [params.listingId]);
+
   return <div>Listing</div>;
 };
 export default Listing;
